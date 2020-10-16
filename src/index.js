3e0f76ac767bf617416de6a35e3a1b134c1a48d9
Nick Frolov <frolovnick0@gmail.com>
2020-10-16 20:35:23 +0300

    init: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+    let array = Array.from(String(n))
+    return array[0] === '-' 
+          ? Number(array.splice(1, array.length).reverse().join(''))
+          : Number(array.reverse().join(''))
+  }
